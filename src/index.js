1f0e9ebce34943d599ead6833158e6a29ad4b736
wakeupmh <wakeupmh@gmail.com>
2019-07-01 16:45:07 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+const element = document.getElementById('root');
+const jsx = <h1>Sarve React!</h1>
+ReactDom.render(jsx, element);
+// ReactDom.render(
+//     <ul>
+//         <li> 1) Dylan </li>
+//         <li> 2) Inaê </li>
+//         <li> 3) Mh </li>
+//     </ul>
+// , element)
+
