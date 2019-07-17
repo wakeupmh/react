9cde0b797f0e3d2c0b7e4acb946486c845009fa8
wakeupmh <wakeupmh@gmail.com>
2019-07-16 23:23:15 -0300

    exploring ibmwatson

---
+++ providers/vrt1.js
@@ -0,0 +1,35 @@
+const VisualRecognitionV3 = require ('watson-developer-cloud/visual-recognition/v3'),
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    fsPromises = fs.promises;
+    
+let visualRecognition = new VisualRecognitionV3 ({
+    version: '2018-03-19', 
+    iam_apikey: '<apiKey>'
+});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+app.post('/whichFruit', async (req,res) => {
+    let name = new Date().getTime();
+    fsPromises.writeFile(`${name}.png`, req.body.img.replace(/^data:image\/\w+;base64,/, ''), {encoding: 'base64'})
+    .then(() => {
+        visualRecognition.classify ({
+            images_file: fs.createReadStream(`${name}.png`), 
+            classifier_ids: ['food']
+        }, function (err, response) {
+            returnClasses(response.images[0].classifiers[0].classes);
+        });
+    })
+    .catch(er => {
+        console.log(er);
+    });
+})
+const returnClasses = classes =>{
+    console.log(classes)
+}
+
