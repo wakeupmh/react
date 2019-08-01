6d9f7020425e0dbc4a28da1212956c8bf71a037d
wakeupmh <wakeupmh@gmail.com>
2019-08-01 00:21:26 -0300

    Todo components

--- todo-app/frontend/src/components/App.jsx
+++ todo-app/frontend/src/components/App.jsx
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faPlus } from '@fortawesome/fontawesome-free-solid'
+import { faCheckSquare } from '@fortawesome/fontawesome-free-solid'
 import React from 'react'
+import Todo from './Todo'
+import About from './About'
 
 export default props =>
     <div className='container'>
-        <h1> Teste</h1>
-        <FontAwesomeIcon icon={faCoffee} />
-        <FontAwesomeIcon icon={faPlus} />
+        <Todo/>
+        <About/>
     </div>
 
 
